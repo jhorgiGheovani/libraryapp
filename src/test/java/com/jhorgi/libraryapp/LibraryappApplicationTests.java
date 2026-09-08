34bc6252d0b032051219e7cdec772651c5a4ebef
Jhorgi <jhorgigheovani@gmail.com>
2026-09-08 22:36:51 +0700

    init project

---
+++ src/test/java/com/jhorgi/libraryapp/LibraryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhorgi.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
